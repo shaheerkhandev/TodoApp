fff6824437c7738ef4a2df57f4653d53c4d4e2bc
shaheerkhandev <77278850+shaheerkhandev@users.noreply.github.com>
2021-01-14 03:21:22 -0800

    Added AwesomeAlert
    - Added AwesomeAlert v1.4.2
    - Switched from UnorderedLists v1.0.4 to using View

--- App.js
+++ App.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
-import {Alert, Text ,View, Button, TextInput, TouchableOpacity, ScrollView} from 'react-native'
+import {Alert, Text ,View, Button, TextInput, TouchableOpacity, ScrollView,} from 'react-native'
 import Icon from 'react-native-vector-icons/MaterialIcons'
-import Unorderedlist from 'react-native-unordered-list';
+import AwesomeAlert from 'react-native-awesome-alerts';
 
 class App extends Component {
   constructor(props) {
@@ -10,14 +10,15 @@ class App extends Component {
     this.state = {
       list: [],
       usertodo: " ",
+      showAlert: false,
     };
   }
 
   onAddItem = () => {
-    if(this.state.usertodo === ""){this.openTwoButtonAlert}else if(this.state.usertodo === " "){this.openTwoButtonAlert}else{
+    if(this.state.usertodo === ""){this.openAlert}else if(this.state.usertodo === " "){this.openAlert}else{
     var newStateArray = this.state.list.slice();
     newStateArray.push({id: this.state.list.length + 1,todo: this.state.usertodo,});
-    this.setState({list: newStateArray});
+    this.setState({list: newStateArray,});
     }
   }
  
@@ -31,18 +32,16 @@ class App extends Component {
     });
   };
 
-  openTwoButtonAlert=()=>{
-    Alert.alert(
-      'No input',
-      'Please enter a todo first.',
-      [
-        {text: 'Yes'},
-        {text: 'No', style: 'cancel'},
-      ],
-      { 
-        cancelable: true 
-      }
-    );
+  openAlert=()=>{
+    this.setState({
+      showAlert: true
+    });
+  }
+
+  closeAlert=()=>{
+    this.setState({
+      showAlert: false
+    });
   }
 
   handleText=(text)=>{
@@ -50,25 +49,45 @@ class App extends Component {
   }
  
   render() {
+    const {showAlert} = this.state;
     return (<>
       <Text style={{position: 'relative', top: 10,fontWeight: 'bold', fontSize: 24, textAlign: 'center'}}>Todo App</Text>
       <View style={{position: 'relative', top: 30,}}>
-      <TextInput placeholder=" Enter Todo..." style={{height: 50}} onChangeText={this.handleText}/>
+      <TextInput placeholder=" Enter Todo..." style={{height: 50}} onChangeText={this.handleText} onSubmitEditing={this.onAddItem}/>
       <Button onPress={this.onAddItem} title="Add" />
+      <AwesomeAlert
+          show={showAlert}
+          showProgress={false}
+          title="Empty Todo"
+          message="Please enter text in the textbox."
+          closeOnTouchOutside={true}
+          closeOnHardwareBackPress={false}
+          showCancelButton={true}
+          showConfirmButton={true}
+          cancelText="Cancel"
+          confirmText="Ok"
+          confirmButtonColor="#DD6B55"
+          onCancelPressed={() => {
+            this.hideAlert();
+          }}
+          onConfirmPressed={() => {
+            this.hideAlert();
+          }}
+        />
       <View style={{position: 'relative', top: 20, left: 10,}}>
-      <ScrollView>
-        <Unorderedlist>
+      <ScrollView style={{ backgroundColor: 'white', marginVertical:20 }} showsVerticalScrollIndicator={true}>
+        <View>
           {this.state.list.map(item => (<>
-            <Unorderedlist key={item.id} style={{paddingBottom: 20}}>
+            <View key={item.id}  style={{borderBottomColor: 'black',borderBottomWidth: 5, marginBottom: 20}}>
               <Text style={{fontSize: 22}}>{item.todo} 
               <TouchableOpacity onPress={() => this.onRemoveItem(item.id)}>
               <Icon name="delete" size={30} />
               {/* <Text style={{fontSize: 14}}>Remove</Text> */}
           </TouchableOpacity>
               </Text>
-            </Unorderedlist>
+            </View>
           </>))}
-        </Unorderedlist>
+        </View>
         </ScrollView>
         </View>
       </View>
